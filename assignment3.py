18c98933e55d3cceb7dda73dad3fcc870431207e
tabishalii <35305133+tabishalii@users.noreply.github.com>
2019-11-23 21:25:47 +0500

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(x, y):
+   return x + y
+# This function subtracts two numbers 
+def subtract(x, y):
+   return x - y
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+# Take input from the user 
+choice = input("Enter choice(1/2/3/4): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
+
+
+# In[3]:
+
+
+X= {0:5, 1:10}
+print(X)
+X.update({2:15})
+print(X)
+
+
+# In[4]:
+
+
+dic = {'value1':1000,'value2':-554,'value3':2047}
+print(sum(dic.values()))
+
+
+# In[5]:
+
+
+def Repeat(x): 
+    _size = len(x) 
+    repeated = [] 
+    for i in range(_size): 
+        k = i + 1
+        for j in range(k, _size): 
+            if x[i] == x[j] and x[i] not in repeated: 
+                repeated.append(x[i]) 
+    return repeated 
+  
+list1 = [10, 20, 30, 20, 20, 30, 40,  
+         50, -20, 60, 60, -20, -20] 
+print (Repeat(list1)) 
+      
+
+
+# In[8]:
+
+
+y = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+def is_key_present(x):
+  if x in y:
+      print('Key is present in the dictionary')
+  else:
+      print('Key is not present in the dictionary')
+is_key_present(5)
+is_key_present(9)
+
+
+# In[ ]:
+
+
+
+
